bfd43a94bf35e10dc43a21bcfd3dac17d9c5ef4b
kumaryoya <kumaryoya05210521@gmail.com>
2024-11-08 19:48:35 +0900

    セットアップ

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
     sessions: 'users/sessions'
-  }, path: '', path_names: {sign_in: 'signin', sign_out: 'signout'}
+  }, path: '', path_names: {sign_in: 'sign_in', sign_out: 'sign_out'}
 
   resources :posts, only: %i[show new create edit update destroy]
 end
