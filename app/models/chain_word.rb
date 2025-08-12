db7b0c95d8b5b2c212c47f37d91860651ada1bd0
kumaryoya <kumaryoya05210521@gmail.com>
2025-08-12 18:28:52 +0900

    セットアップ

--- app/models/chain_word.rb
+++ app/models/chain_word.rb
@@ -5,4 +5,25 @@ class ChainWord < ApplicationRecord
 
   validates :word, presence: true, uniqueness: true, length: { maximum: 255 },
                    format: { with: /\A[\p{Hiragana}]+\z/, message: 'はひらがなのみで入力してください' }
+
+  validate :chain_word_rule
+
+  private
+
+  def chain_word_rule
+    return if word.blank?
+
+    last_chain_word = ChainWord.order(:created_at).last
+    return if last_chain_word.nil?
+
+    last_word = last_chain_word.word
+    return if last_word.blank?
+
+    last_char = last_word.chars.last
+    first_char = word.chars.first
+
+    return if last_char == first_char
+
+    errors.add(:word, "「#{last_char}」で始まる単語を入力してください")
+  end
 end
